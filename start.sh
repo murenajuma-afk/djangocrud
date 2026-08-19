9f2118d6a1f1e2863891cea9984963c1b4b9435a
murenajumaafk <murenajuma@gmail.com>
2026-08-19 12:16:55 +0300

    crud

--- start.sh
+++ start.sh
@@ -2,5 +2,5 @@ set -e
 cd myproject
 python manage.py migrate --noinput
 python manage.py create_deploy_superuser || true
-python manage.py collecstatic --noinput
+python manage.py collectstatic --noinput
 exec gunicorn myproject.wsgi --bind 0.0.0.0:${PORT:-8000}
